0178f9625189c51cc34c1c42ad16fab44aa950aa
Jason Coene <jcoene@gmail.com>
2013-07-17 15:24:45 -0500

    Initial release

---
+++ README.md
@@ -0,0 +1,23 @@
+# StatsD Client
+
+This is a no-nonsense StatsD client written in Go. It reconnects automatically if the write socket fails, making it at least 1000% better than any other StatsD client available.
+
+It doesn't support sampling, I'd welcome a good pull request.
+
+Pair it with [statsd-librato server](https://github.com/jcoene/statsd-librato) for a simple and efficient pipeline to Librato.
+
+## Usage
+
+```go
+import "github.com/jcoene/statsd-client"
+
+client, err := statsd.NewClient("127.0.0.1:8125", "myapp")
+
+client.Inc("http.response.200.count", 1)        // myapp.http.response.200.count:1|c
+client.Timing("http.response.200.runtime", 50)  // myapp.http.response.200.runtime:50|ms
+client.Gauge("server.load", 5)                  // myapp.server.load:5|g
+```
+
+## License
+
+MIT License, see LICENSE file.
---
+++ statsd.go
@@ -0,0 +1,80 @@
+package statsd
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"sync"
+)
+
+type Client struct {
+	Addr   string
+	Prefix string
+	nc     net.Conn
+	mu     sync.Mutex
+	rw     *bufio.ReadWriter
+}
+
+func NewClient(addr string, prefix string) (c *Client, err error) {
+	c = &Client{
+		Addr:   addr,
+		Prefix: prefix,
+	}
+
+	err = c.redial()
+
+	return
+}
+
+func (c *Client) Inc(k string, d int64) error {
+	m := fmt.Sprintf("%s:%d|c", c.prefix(k), d)
+	return c.send([]byte(m))
+}
+
+func (c *Client) Dec(k string, d int64) error {
+	return c.Inc(k, -d)
+}
+
+func (c *Client) Gauge(k string, v int64) error {
+	m := fmt.Sprintf("%s:%d|g", c.prefix(k), v)
+	return c.send([]byte(m))
+}
+
+func (c *Client) Timing(k string, v int64) error {
+	m := fmt.Sprintf("%s:%d|ms", c.prefix(k), v)
+	return c.send([]byte(m))
+}
+
+func (c *Client) redial() (err error) {
+	c.nc = nil
+	if c.nc, err = net.Dial("udp", c.Addr); err != nil {
+		return
+	}
+
+	c.rw = bufio.NewReadWriter(bufio.NewReader(c.nc), bufio.NewWriter(c.nc))
+
+	return
+}
+
+func (c *Client) send(data []byte) (err error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	_, err = c.rw.Write(data)
+	if err != nil {
+		fmt.Println("redialing")
+		c.redial()
+	}
+
+	err = c.rw.Flush()
+
+	return
+}
+
+func (c *Client) prefix(k string) string {
+	if c.Prefix == "" {
+		return k
+	}
+
+	return fmt.Sprintf("%s.%s", c.Prefix, k)
+}
